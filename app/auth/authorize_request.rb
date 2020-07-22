eb6ebbbd7aecca238f3d80a0f37fe8bb7163d55e
damidurodola <oluwadamilola.durodola@andela.com>
2020-07-22 23:37:42 +0100

    implement authentication of users

---
+++ app/auth/authorize_request.rb
@@ -0,0 +1,37 @@
+class AuthorizeRequest
+
+  def initialize(headers = {})
+    @headers = headers
+  end
+
+  def call
+    {
+      user: user
+    }
+  end
+
+  private
+  attr_reader :headers
+
+  def user
+    @user ||= User.find(decoded_auth_token[:user_id]) if decoded_auth_token
+
+  rescue ActiveRecord::RecordNotFound => e
+    raise(
+      "#{e.message}"
+    )
+  end
+
+  def decoded_auth_token
+    @decoded_auth_token ||= JsonWebToken.decode(http_auth_header)
+
+  rescue JWT::DecodeError => e
+    raise ("#{e.message}")
+  end
+
+  def http_auth_header
+    if headers['Authorization'].present?
+      return headers['Authorization'].split(' ').last
+    end
+  end
+end
